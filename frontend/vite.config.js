5fe5f8f2913160119461974f05650a2e2cd293ee
mohamed islam <mohameddjabri865@gmail.com>
2026-04-09 21:24:02 +0100

    fourth commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,25 +1,15 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
-import path from "path"
 // https://vite.dev/config/
-export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return defineConfig({
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://netflix-clone-backend-three.vercel.app",
+        changeOrigin: true,
       },
     },
-    server: {
-      proxy: {
-        '/api': {
-          target: env.VITE_API_BASE_URL,
-          changeOrigin: true,
-        },
-      },
-    },
-  })
-}
+  },
+});
